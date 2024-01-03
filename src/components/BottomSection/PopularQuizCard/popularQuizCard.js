3d41928d8c1ced1a48d816b3aa89a8be08e7b7a6
Raphael <barreiroraphael@gmail.com>
2024-01-03 16:32:04 -0300

    Implemented dynamic Detail Screen (modal)

--- src/components/BottomSection/PopularQuizCard/popularQuizCard.js
+++ src/components/BottomSection/PopularQuizCard/popularQuizCard.js
@@ -5,23 +5,35 @@ import ballIcon from './assets/ball-icon.svg'
 import musicIcon from './assets/music-icon.svg'
 import starIcon from './assets/star-icon.svg'
 
-export default function PopularQuizCard( {title, questions, bgColor, rate} ) {
+export default function PopularQuizCard( {title, questions, bgColor, rate, maxTime, setShowDetailsModal, setSelectedCard, blockClick} ) {
     const icons = {
         'technology': techIcon,
         'sport': ballIcon,
         'music': musicIcon
     }
+    const handleCardClick = (e) => {
+        const card = {
+            "title": title,
+            "questions": questions,
+            "bgColor": bgColor,
+            "rate": rate,
+            "maxTime": maxTime
+        };
+        setSelectedCard(card);
+        window.scrollTo(0, 0);
+        setShowDetailsModal(true);
+    };
     return (
-    <div className={styles.card}>
-        <div  className={styles.icon_container} style={{backgroundColor: bgColor}}>
-            <img src={icons[title.toLowerCase()]} alt={`Popular Quiz ${title} icon`} />
+    <div className={styles.card} onClick={(e) => handleCardClick(e)} style={{pointerEvents: blockClick ? 'none' : 'auto'}}>
+        <div className={styles.icon_container} style={{backgroundColor: bgColor}}>
+            <img src={icons[title ? title.toLowerCase() : '']} alt={`Popular Quiz ${title} icon`} />
         </div>
         <div className={styles.texts}>
             <h3>{title}</h3>
             <p>{questions} Question</p>
         </div>
         <div className={styles.rating}>
-            <img src={starIcon} alt='Start icon refering to rating note'/>
+            <img src={starIcon} alt='Star icon refering to rating note'/>
             <span>{rate}</span>
         </div>
     </div>
--- src/components/BottomSection/bottomSection.js
+++ src/components/BottomSection/bottomSection.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './BottomSection.module.css'
 import PopularQuizCard from './PopularQuizCard/popularQuizCard'
+import QuizDetails from '../modals/QuizDetails/quizDetails'
 
 export default function BottomSection() {
+  const [showDetailsModal, setShowDetailsModal] = useState(false);
+  const [selectedCard, setSelectedCard] = useState({});
+  const popularQuizCards = [
+    {"id": "1", "title": "Technology", "questions": 10, "bgColor": "var(--bg-icon-yellow)", "rate": 4.8, 'maxTime': 120},
+    {"id": "2", "title": "Sport", "questions": 10, "bgColor": "var(--bg-icon-blue)", "rate": 4.8, 'maxTime': 60},
+    {"id": "3", "title": "Music", "questions": 10, "bgColor": "var(--bg-icon-red)", "rate": 4.8, 'maxTime': 45},
+  ];
+
+
   return (
     <div className={styles.bottom_section}>
-        <h2 className={styles.h2}>Popular Quiz</h2>
-        <div className={styles.cards}>
-            <PopularQuizCard title="Technology" questions="10" bgColor="var(--bg-icon-yellow)" rate="4.8" />
-            <PopularQuizCard title="Sport" questions="10" bgColor="var(--bg-icon-blue)" rate="4.8" />
-            <PopularQuizCard title="Music" questions="10" bgColor="var(--bg-icon-red)" rate="4.8" />
-        </div>
+      <h2 className={styles.h2}>Popular Quiz</h2>
+      <div className={styles.cards}>
+        {popularQuizCards.map(card => (
+          <PopularQuizCard
+            key={card.id}
+            id={card.id}
+            title={card.title}
+            questions={card.questions}
+            bgColor={card.bgColor}
+            rate={card.rate}
+            maxTime={card.maxTime}
+            showDetailsModal={showDetailsModal}
+            setShowDetailsModal={setShowDetailsModal}
+            setSelectedCard={setSelectedCard}
+            selectedCard={selectedCard}
+          />
+        ))}
+        {showDetailsModal ?
+          <QuizDetails
+            title={selectedCard.title}
+            questions={selectedCard.questions}
+            maxTime={selectedCard.maxTime}
+            bgColor={selectedCard.bgColor}
+            rate={selectedCard.rate}
+            setShowDetailsModal={setShowDetailsModal}
+            modalTitle ='Detail Quiz'
+          />
+        : ''}
+      </div>
     </div>
-  )
+  );
 }
